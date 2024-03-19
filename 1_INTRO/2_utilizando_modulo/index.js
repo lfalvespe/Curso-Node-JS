c21b9276693830118901df6b2bc5c0a883c7767c
Fernando Alves <lfalvespe@gmail.com>
2024-03-19 15:48:10 -0300

    introduction

---
+++ 1_INTRO/2_utilizando_modulo/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs') // file system
+
+fs.readFile('arquivo.txt', 'utf8', (err, data) => {
+
+    if(err) {
+        console.log(err.message)
+        return
+    } 
+
+    console.log(data)
+
+})
